c4ac5625f362c42cba959dbf4ab07c831d5af0c9
SuyogPalav <suyogpalav5@gmail.com>
2025-02-18 21:30:57 +0530

    Added School.sql database file.

---
+++ School.sql
@@ -0,0 +1,14 @@
+create database school1;
+use school1;
+
+drop database school1;
+DROP TABLE jpa_laptop;
+
+show tables;
+
+select * from jpa_student;
+select * from jpa_laptop;
+select * from jpa_address;
+select * from jpa_category;
+select * from jpa_product;
+select * from jpa_category_product;
